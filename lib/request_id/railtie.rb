16fefd3b75506f916e32deb3c22adede67028b78
Paweł Kondzior <pawel@kondzior.com>
2012-11-27 23:39:17 +0100

    Put Railtie into module

--- lib/request_id/railtie.rb
+++ lib/request_id/railtie.rb
@@ -1,5 +1,7 @@
-class RequestId < Rails::Railtie
-  initializer "request_id.configure_rails_initialization" do |app|
-    app.middleware.use RequestId::Middleware
+module RequestId
+  class Railtie < Rails::Railtie
+    initializer "request_id.configure_rails_initialization" do |app|
+      app.middleware.use RequestId::Middleware
+    end
   end
 end
